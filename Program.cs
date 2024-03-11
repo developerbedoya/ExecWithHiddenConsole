95a5f2ec6144193ab1b67e65f827da220e08981b
Daniel Bedoya Alzate <dbalzate@gmail.com>
2024-03-11 12:22:35 -0300

    First commit.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ExecWithHiddenConsole;
+
+class Program
+{
+    static void Main(string[] args) => VerifyArgs(args, () => ExecWithHiddenConsole(args[0], args.Skip(1).ToArray()));
+
+    [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr h, string m, string c, int type);
+
+    private static void VerifyArgs(string[] args, Action action)
+    {
+        if (args.Length < 1)
+        {
+            MessageBox((IntPtr)0,"Usage: ExecWithHiddenConsole <program> <args>", "ExecWithHiddenConsole", 0);
+            return;
+        }
+
+        action?.Invoke();
+    }
+
+    static void ExecWithHiddenConsole(string program, string[] args)
+    {
+        var process = new Process();
+        process.StartInfo.FileName = program;
+        process.StartInfo.Arguments = string.Join(" ", args);
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ExecWithHiddenConsole
-Open a console application without showing their console window
+Open a console application without showing their console window. This application only works in Windows.
+
+# How to use
+```ExecHiddenConsole.exe <c:\path\to\console\application.exe> <arguments>```
